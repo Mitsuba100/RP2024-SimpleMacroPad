37be5605d5334559c6990ea4bf495b9a25bdc850
Mitsuba100 <82495539+Mitsuba100@users.noreply.github.com>
2025-05-27 15:35:42 +0200

    Update my4pad.h

--- my4pad/my4pad.h
+++ my4pad/my4pad.h
@@ -1,20 +1,7 @@
-#pragma once
+#include QMK_KEYBOARD_H
 
-#include "quantum.h"
-
-/* This a shortcut to help you visually see your layout.
- *
- * The first section contains all of the arguments representing the physical
- * layout of the board and position of the keys.
- *
- * The second converts the arguments into a two-dimensional array which
- * represents the switch matrix.
- */
-
-#define LAYOUT( \
-    k00, k01, k02, k03, k04 \
-  ) \
-  { \
-    { k00, k01, k02, k03, k04 } \
-  }
-
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_A
+    )
+};
